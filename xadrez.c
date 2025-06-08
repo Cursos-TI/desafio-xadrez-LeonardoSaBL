fe3032f9067f3b53e618db8dfad3b57810b13bf0
LeonardoSaBL <202501358251@alunos.estacio.br>
2025-06-08 23:26:58 +0000

    Desaafio batalha naval

--- xadrez.c
+++ xadrez.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 
-int main(){
+#define TAMANHO 10
+#define TAMANHO_NAVIO 3
 
-    int t = 0,b = 0,r = 0;
+int main() {
+    int tabuleiro[TAMANHO][TAMANHO];
 
-    printf("A movimentação da peça Torre \n");
-
-    do {
-
-        printf("  Direita\n");
-
-
-        t++;
-
-
-     } while (t < 5);
-
-    printf(" A  movimentação da peça Bispo é: \n");
-
-    while(b < 5) {
-
-        printf("Cima, Direita\n");
-
-        b++;
-
-
-
-
+    // Inicializa o tabuleiro com 0 (água)
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            tabuleiro[i][j] = 0;
+        }
     }
 
-    printf("A movimentação da Rainha é: \n");
+    // Criação dos navios (representados por vetores)
+    int navio_horizontal[TAMANHO_NAVIO] = {3, 3, 3};
+    int navio_vertical[TAMANHO_NAVIO] = {3, 3, 3};
 
-    for (r = 0; r < 8; r++) {
+    // Coordenadas iniciais dos navios
+    int linha_horizontal = 2;  // Linha fixa
+    int coluna_inicial_horizontal = 4; // Coluna inicial
 
-        printf("Esquerda\n");
+    int coluna_vertical = 7;  // Coluna fixa
+    int linha_inicial_vertical = 1; // Linha inicial
 
+    // Posiciona navio horizontal (na linha 2)
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linha_horizontal][coluna_inicial_horizontal + i] = navio_horizontal[i];
     }
 
-    
+    // Posiciona navio vertical (na coluna 7)
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linha_inicial_vertical + i][coluna_vertical] = navio_vertical[i];
+    }
 
+    // Exibe o tabuleiro
+    printf("=== TABULEIRO DE BATALHA NAVAL ===\n\n");
+    for (int i = 0; i < TAMANHO; i++) {
+        for (int j = 0; j < TAMANHO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
-
 }
